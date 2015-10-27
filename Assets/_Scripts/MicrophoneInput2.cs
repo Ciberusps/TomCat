51191941dcf708f5eac574771e79f2ab5bb5bced
Ciberus <Ciberus.ps@gmail.com>
2015-10-27 18:06:51 +0300

    -Опеределение громкости -Добавлен файл gitignore

---
+++ Assets/_Scripts/MicrophoneInput2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput2 : MonoBehaviour
+{
+    public float sensitivity = 100;
+    public float loudness = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    audio.clip = Microphone.Start(null, true, 10, 44100);
+	    audio.loop = true;
+        audio.mute = true;
+        while (!(Microphone.GetPosition(null) > 0))
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        loudness = GetAveragedVolume()*sensitivity;
+    }
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+
+        audio.GetOutputData(data, 0);
+        for (var i = 0; i < data.Length; i++)
+        {
+            a += Mathf.Abs(data[i]);
+        }
+
+        return a/256;
+    }
+}
---
+++ Assets/_Scripts/TomCatV2.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class TomCatV2 : MonoBehaviour
+{
+    public GameObject audioInputObject;
+    public float startRecordThreshold = 1F;
+    public float endRecordThreshold = 1F;
+    public Vector2 pitchSlowBound;
+    public Vector2 pitchFastBound;
+
+    //    private MicrophoneInput _micIn;
+    public MicControlC micCon;
+
+    private bool _recordVoice = false;
+    private float l;
+    private bool _stopRecord = false;
+    void Start()
+    {
+        /*if (audioInputObject != null)
+            _micIn = audioInputObject.GetComponent<MicrophoneInput>();*/
+    }
+
+    void Update()
+    {
+        /* if (!_recordVoice)
+         {
+             float l = _micIn.loudness;
+
+             if (l > startRecordThreshold)
+             {
+                 _StartVoiceRecord();
+             }
+         }
+         else
+         {
+             float l = _micIn.loudness;
+
+             if (l < endRecordThreshold)
+             {
+                 _EndVoiceRecord();
+             }
+         }*/
+
+
+        l = micCon.loudness;
+
+
+        if (!_stopRecord)
+        {
+            if (!_recordVoice)
+            {
+                if (l > startRecordThreshold)
+                    _StartVoiceRecord();
+            }
+            else
+            {
+                if (l < endRecordThreshold)
+                    _EndVoiceRecord();
+            }
+        }
+        
+
+        print(l);
+    }
+
+    public void _StartVoiceRecord()
+    {
+        _recordVoice = true;
+        audio.clip = Microphone.Start(null, false, 30, 44100);
+
+        Debug.LogWarning("StartRecord " + micCon.loudness);
+    }
+
+    public void _EndVoiceRecord()
+    {
+        _recordVoice = false;
+        Microphone.End(null);
+
+        Debug.LogWarning("EndRecord " + micCon.loudness);
+
+        _stopRecord = true;
+
+        _HandleSound();
+        Play();
+    }
+
+    public void Play()
+    {
+        audio.Play();
+        audio.loop = false;
+        Debug.LogWarning("play " + micCon.loudness);
+    }
+
+    private void _HandleSound()
+    {
+        var randNum = Random.Range(0, 2);
+        audio.pitch =
+            randNum == 0
+                ? Random.Range((float)pitchSlowBound.x, pitchSlowBound.y)
+                : Random.Range((float)pitchFastBound.x, pitchFastBound.y);
+    }
+
+    public void RestartMicroRecord()
+    {
+        micCon.StartMicrophone();
+        _stopRecord = false;
+    }
+}
